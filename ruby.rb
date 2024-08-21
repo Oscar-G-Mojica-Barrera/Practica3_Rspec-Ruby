3328b81ec2dbce6534752e4d76cd051421a5bdea
Oscar-G-Mojica-Barrera <oscar.mojica121@est.unanleon.edu.ni>
2024-08-21 16:38:16 -0600

    Add files via upload

---
+++ ruby.rb
@@ -0,0 +1,15 @@
+def intro array
+    #escribir el código
+    return 0 if array.empty?
+    array.sum
+end
+def nombre nom
+    return "vacio" if nom.empty?
+    #escribir el código
+    "hello #{nom.capitalize}"
+
+end
+def datos? array
+    #escribir el código
+    array.size> 0
+end
